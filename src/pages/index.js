73f76832fa25a82e5bc271e62bafd5fd19ef0c67
rinsgray <batyamaxno@gmail.com>
2020-07-17 15:26:43 +0400

    Gatsby init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import Header from "../components/header"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+    <Header headerText = 'Index page' />
+    <Link to='/about/'>About</Link>
+
+  </div>
 }
