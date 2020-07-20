af238533f3ffdd364fb0aa9a4b5da3ebb736a8a9
rinsgray <batyamaxno@gmail.com>
2020-07-20 17:29:23 +0400

    cleaned-up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
-import Header from "../components/header"
 import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
   return <div>
-    <Header headerText = 'Index page' />
-    <Link to='/about/'>About</Link>
-
+    <Header headerText="This home page" />
+    <Link to = '/contact/'><p>Contact</p></Link>
+    <Link to = '/about/'><p>About Us</p></Link>
   </div>
 }
