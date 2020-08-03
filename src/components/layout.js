8e7da7bdc9b44ce53990581b28a28d67c7f351f8
DanyaYashin <yashindan2000@gmail.com>
2020-08-03 17:57:04 +0400

    layout css

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
 import Header from "./header"
+import layoutStyles from "./layout.module.css"
 
 export default function Layout(props) {
   return (
     <div>
-    <div class="indexhdr">
+    <div class='indexhdr'>
       <Header headerText={props.header} />
       <Link to = '/'><p>Home</p></Link>
       <Link to = '/contact/'><p>Contact</p></Link>
       <Link to = '/about/'><p>About Us</p></Link>
     </div>
-    <div style={{ margin: `3rem auto`, padding: `0 1rem` }}>
+    <div class='menu'>
       {props.children}
     </div>
     </div>
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,15 +2,16 @@ import React from "react"
 import Header from "../components/header"
 import Container from "../components/container"
 import { Link } from "gatsby"
+import Layout from "../components/layout"
 
 export default function About() {
   return (
+    <Layout header="About Gatsby">
     <Container>
     <div style={{ color: 'black' }}>
-    <Header headerText = 'About Gatsby' />
       <p>Such wow. Very React.</p>
-    <Link to='/'><p>Home</p></Link>
     </div>
     </Container>
+    </Layout>
   )
 }
